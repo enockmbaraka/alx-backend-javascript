bc908aae47175cf87f5e2d58f02fe34ee0d17a1e
enockmbaraka <enockmwanzia17@gmail.com>
2024-10-14 14:38:54 +0300

    manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,13 @@
-export { weakMap, queryAPI };const weakMap = new WeakMap();
-const queryAPI = (endpoint) => {
-  const counter = (weakMap.get(endpoint) || 0) + 1;
-  if (counter >= 5) throw new Error('Endpoint load is hight');
-  weakMap.set(endpoint, counter);
-};
-export { weakMap, queryAPI };
+export const weakMap = new WeakMap();
+
+const MAX_ENDPOINT_CALLS = 5;
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
+  }
+}
