b695e8f5341e6c72e0f84a2a180a560c61ea43a8
enockmbaraka <enockmwanzia17@gmail.com>
2024-10-14 13:37:05 +0300

    classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,32 +1,59 @@
 import Currency from './3-currency';
 
-export default class Pricing {
+class Pricing {
   constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  /**
+   * @param {Number} amount
+   */
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a Number');
+    }
     this._amount = amount;
-    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(cash) {
-    this._amount = cash;
+  /**
+   * @param {Currency} currency
+   */
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('currency must be an instance of Currency');
+    }
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(cur) {
-    this._currency = cur;
-  }
-
   displayFullPrice() {
     return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
+  /**
+   * @param {Number} amount
+   * @param {Number} conversionRate
+   */
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+
     return amount * conversionRate;
   }
 }
+
+export default Pricing;
